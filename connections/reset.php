19a6e5ef0f1ce632c307244b879981cd9215950c
sofiacarballo <sofia.carballo@live.com>
2020-03-24 15:48:04 +0100

    New files

---
+++ connections/reset.php
@@ -0,0 +1,9 @@
+<?php
+use App\Connection;
+include ("connect.php");
+
+$sql = " UPDATE coders_list SET status = 'Alive' WHERE status = ALL(SELECT status FROM coders_list WHERE status = 'Dead')";
+$result = $connection->query($sql);
+
+$connect->close();
+?>
--- connections/update.php
+++ connections/update.php
@@ -1,11 +1,10 @@
 <?php
 include ("connect.php");
+include ("mysqlRandom.php");
 
-$sql = "UPDATE coders_list SET status='Alive' WHERE id=1";
-    if(mysqli_query($connect,$sql)){
-        echo "OK Update.";
-    } else {
-        echo "ERROR: No Update. ";
-}
+$sql = "UPDATE coders_list SET status='Dead' WHERE id='$id' ";
+$result= $connect->query($sql);
+
+$connect->close();
 
 ?>
