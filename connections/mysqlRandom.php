c069a1cc3a43bb61bfabc501206b0767bd46a1f0
JoseOrtegaVillen <58030778+JoseOrtegaVillen@users.noreply.github.com>
2020-03-24 14:11:16 +0100

    Update Files
    Add files:
    - Counter ('Alive' , 'Dead')
    - List ('Alive , 'Dead')
    - Select random to coder
    - Reset status
    - Update status 'Dead'
    - Add coder with status 'Alive'
    - Connection with db mysql

--- connections/mysqlRandom.php
+++ connections/mysqlRandom.php
@@ -3,9 +3,10 @@ include("connect.php");
 
 $query="SELECT * FROM coders_list WHERE status='Alive'ORDER BY RAND() LIMIT 1";
 $result= $connect->query($query);
-
 while($row=$result->fetch_assoc()){
+$id=$row['id'];
 
+echo $row['name'];
 return $row['id'];
 
 header("Location: http://localhost/phpfactoriaf5/wheelOfDoom/index.php"); die();
---
+++ connections/reset.php
@@ -0,0 +1,6 @@
+<?php
+use App\Connection;
+$connection = new mysqli("127.0.0.1", "root", "", "coders");
+
+$sql = " UPDATE coders_list SET status = 'Alive' WHERE status = ALL(SELECT status FROM coders_list WHERE status = 'Dead')";
+$result = $connection->query($sql);
